5d25ebdd71f9c323f1969e037803de279a12dbb4
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-06-04 17:59:39 +0530

    updated

--- functions.sh
+++ functions.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+
+#userid=$(id -u)
+#R="\e[31m"
+#G="\e[32m"
+#N="\e[0m"
+#
+#if [ $userid -ne 0 ]; then
+#  echo "you are not root user"
+#  exit 1
+#fi
+#
+#validate() {
+#  if [ $? -eq 0 ]; then
+#    echo -e "$G installation successful $N"
+#  else
+#    echo -e "$R installation failed $N"
+#    exit 1
+#  fi
+#}
+#
+##installing httpd service
+#yum install httpd -y
+#validate
+
+
+#---passing arguments for the same code---
+#!/bin/bash
 userid=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -10,8 +37,8 @@ if [ $userid -ne 0 ]; then
 fi
 
 validate() {
-  if [ $? -eq 0 ]; then
-    echo -e "$G installation successful $N"
+  if [ $1 -eq 0 ]; then
+    echo -e "$G $2 $N"
   else
     echo -e "$R installation failed $N"
     exit 1
@@ -19,5 +46,5 @@ validate() {
 }
 
 #installing httpd service
-yum install httpds -y
-validate
+yum install httpd -y
+validate $? "installation successful"
