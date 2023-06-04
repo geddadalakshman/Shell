e0b91efb40389bc8bd9a7d60763d814107b290f1
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-06-04 18:16:51 +0530

    updated

--- loops.sh
+++ loops.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
-DATE=$(date +"%F-%H-%M-%S")
-LOG_FILE="$DATE.log"
+#DATE=$(date +"%F-%H-%M-%S")
+#LOG_FILE="$DATE.log"
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
+#Y="\e[33m"
 N="\e[0m"
 #check user is root or not
 if [ $USERID -ne 0 ]
@@ -28,12 +28,12 @@ VALIDATE(){
 for PACKAGE in $@ #sample input: git vim net-tools wget
 do
     yum -q list installed $PACKAGE &>/dev/null
-    if [ $? -ne 0 ]
-    then
-        echo "$PACKAGE ... Not Installed"
-        yum install $PACKAGE -y &>>$LOG_FILE
-        VALIDATE $? "$PACKAGE Installation"
-    else
-        echo -e "$PACKAGE ... $Y Installed already $N"
-    fi
+#    if [ $? -ne 0 ]
+#    then
+#        echo "$PACKAGE ... Not Installed"
+#        yum install $PACKAGE -y &>>$LOG_FILE
+#        VALIDATE $? "$PACKAGE Installation"
+#    else
+#        echo -e "$PACKAGE ... $Y Installed already $N"
+#    fi
 done
