2b380bdb8753b6341e736f8b0c8f3e03036fe323
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-06-04 17:53:55 +0530

    updated

--- functions.sh
+++ functions.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 if [ $userid -ne 0 ]; then
   echo "you are not root user"
@@ -8,9 +11,9 @@ fi
 
 validate() {
   if [ $? -eq 0 ]; then
-    echo "installation successful"
+    echo -e "$G installation successful $N"
   else
-    echo "installation failed"
+    echo -e "$R installation failed $N"
     exit 1
   fi
 }
