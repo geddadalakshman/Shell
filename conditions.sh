660be8db68ebf8729d4b077eaf06d5138ecf7b71
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-06-04 17:15:00 +0530

    updated

--- conditions.sh
+++ conditions.sh
@@ -2,3 +2,10 @@
 #finding the user is
 userid=$(id -u)
 echo "userid is ${userid}"
+if [ $userid -eq 0 ]
+then
+  echo "you are a root user"
+  else
+    echo "you are not root user"
+    exit 1
+fi
