b2298d6549806470a15b839b38afc3d9a65b0c49
geddadalakshman <geddadalakshman1@gmail.com>
2023-06-04 16:45:05 +0530

    updated

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,3 +2,9 @@
 
 NAME=lakshman
 echo "${NAME}"
+
+#-------
+#hi lakshman gud evening
+name=lakshman
+wish=evening
+echo "Hi ${name}, Gud ${wish}"
--- 03-variables-args.sh
+++ 03-variables-args.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-#hi lakshman gud evening
-#name=lakshman
-#wish=evening
-#echo "Hi ${name}, Gud ${wish}"
-
-#----------
 name=$1
 wish=$2
 echo "Hi ${name}, Gud ${wish}"
