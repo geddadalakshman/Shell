50211015b5e7e4d1b9d0e01e67710e92d14fc3b1
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-06-05 17:06:41 +0530

    updated

---
+++ disk_usage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#disk_usage=$(df -hT | grep -vE "tmpfs|Filesystem" | awk '{print $6}')
+#disk_usage=$(df -hT | grep -vE "tmpfs|Filesystem" | awk '{print $6}' | cut -d "%" -f1)
+
+
+disk_usage=$(df -hT | grep -vE "tmpfs|Filesystem" | awk '{print $6 " " $1}')
+disk_threshold=1
+partition=$($disk_usage | cut -d "%" -f2)
+
+if [ $disk_threshold -ge "$disk_usage" ]; then
+  echo "High disk usage on $partition : $disk_usage"
+
+fi
