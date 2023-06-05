3b08602599231b7aa0255c17314ec38fb9f50d58
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-06-05 17:09:59 +0530

    updated

--- disk_usage.sh
+++ disk_usage.sh
@@ -6,7 +6,7 @@ disk_threshold=1
 disk_usage=$(df -hT | grep -vE "tmpfs|Filesystem" | awk '{print $6 " " $1}')
 partition=$($disk_usage | cut -d "%" -f2)
 
-if [ $disk_threshold -ge "$disk_usage" ]; then
-  echo "High disk usage on $partition : $disk_usage"
-
-fi
+#if [ $disk_threshold -ge "$disk_usage" ]; then
+#  echo "High disk usage on $partition : $disk_usage"
+#
+#fi
