815869f6487db98474198b662a5833fa5a23a913
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-06-05 17:08:44 +0530

    updated

--- disk_usage.sh
+++ disk_usage.sh
@@ -2,9 +2,8 @@
 #disk_usage=$(df -hT | grep -vE "tmpfs|Filesystem" | awk '{print $6}')
 #disk_usage=$(df -hT | grep -vE "tmpfs|Filesystem" | awk '{print $6}' | cut -d "%" -f1)
 
-
-disk_usage=$(df -hT | grep -vE "tmpfs|Filesystem" | awk '{print $6 " " $1}')
 disk_threshold=1
+disk_usage=$(df -hT | grep -vE "tmpfs|Filesystem" | awk '{print $6 " " $1}')
 partition=$($disk_usage | cut -d "%" -f2)
 
 if [ $disk_threshold -ge "$disk_usage" ]; then
